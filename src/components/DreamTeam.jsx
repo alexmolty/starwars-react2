925ddca22f36787147cfe7a118f9ff5ead46b9ea
alexmolt <alexmolt@mail.ru>
2025-07-03 22:10:11 +0300

    DreamTeam images optimisation

--- src/components/DreamTeam.jsx
+++ src/components/DreamTeam.jsx
@@ -1,26 +1,10 @@
-import friend1 from "../assets/friend1.jpg";
-import friend2 from "../assets/friend2.jpg";
-import friend3 from "../assets/friend3.jpg";
-import friend4 from "../assets/friend4.jpg";
-import friend5 from "../assets/friend5.jpg";
-import friend6 from "../assets/friend6.jpg";
-import friend7 from "../assets/friend7.jpg";
-import friend8 from "../assets/friend8.jpg";
-import friend9 from "../assets/friend9.jpg";
+import FriendsImages from "./FriendsImages.jsx";
 
 const DreamTeam = () => {
     return (
         <section className="float-end row w-50 border rounded-bottom-4 me-0 ms-2">
             <h2 className="text-center">Dream team</h2>
-            <img className="col-sm-4 p-1" src={friend1} alt="Friend"/>
-            <img className="col-sm-4 p-1" src={friend2} alt="Friend"/>
-            <img className="col-sm-4 p-1" src={friend3} alt="Friend"/>
-            <img className="col-sm-4 p-1" src={friend4} alt="Friend"/>
-            <img className="col-sm-4 p-1" src={friend5} alt="Friend"/>
-            <img className="col-sm-4 p-1" src={friend6} alt="Friend"/>
-            <img className="bottomLeft col-sm-4 p-1" src={friend7} alt="Friend"/>
-            <img className="col-sm-4 p-1" src={friend8} alt="Friend"/>
-            <img className="bottomRight col-sm-4 p-1" src={friend9} alt="Friend"/>
+            <FriendsImages/>
         </section>
     );
 };
---
+++ src/components/FriendsImages.jsx
@@ -0,0 +1,28 @@
+import {dreamTeam} from "../utils/constants.js";
+
+const FriendsImages = () => {
+    return (
+        <>
+            {dreamTeam.map((friend, index) => {
+                    let className = "col-sm-4 p-1";
+
+                    if (index === 6) {
+                        className += " bottomLeft";
+                    } else if (index === 8) {
+                        className += " bottomRight";
+                    }
+                    return (
+                        <img
+                            key={friend}
+                            src={friend}
+                            className={className}
+                            alt="Friend"
+                        />
+                    )
+                }
+            )}
+        </>
+    );
+};
+
+export default FriendsImages;
